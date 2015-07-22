a3127e5f6795633ef151cc625be0bac8b3c6b915
Rafael Querino <rafaelqm@gmail.com>
2015-07-22 14:14:44 -0300

    PI-52

--- app/Http/Requests/Admin/UserRequest.php
+++ app/Http/Requests/Admin/UserRequest.php
@@ -18,6 +18,19 @@ class UserRequest extends FormRequest {
 		];
 	}
 
+	public function messages(){
+		return [
+			'name.required' => 'É necessário um nome',
+			'name.min' => 'É necessário um nome com no mínimo 3 caracteres',
+			'email.required' => 'O campo de e-mail é necessário.',
+			'email.email' => 'O campo de e-mail precisa ser válido!',
+			'email.unique' => 'O campo de e-mail precisa ser único no sistema, e este já está sendo utilizado!',
+			'password.required' => 'É necessário uma senha.',
+			'password.confirmed' => 'A senha e a confirmação precisam ser iguais.',
+			'password.min' => 'A senha precisa ter no mínimo 5 caracteres.',
+		];
+	}
+
 	/**
 	 * Determine if the user is authorized to make this request.
 	 *
