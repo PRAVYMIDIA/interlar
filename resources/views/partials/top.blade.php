6fdbbea46f549aaa91718e2d10cc1d0a1dc6e87f
Rafael Querino <rafaelqm@gmail.com>
2015-09-04 18:49:34 -0300

    Ajuste de icone do menu

--- resources/views/partials/top.blade.php
+++ resources/views/partials/top.blade.php
@@ -6,15 +6,15 @@
   <div class="container">
     <div class="row">
       <div class="col-xs-12 row">
-        <div class="col-md-3 col-xs-12">
+        <div class="col-md-3 col-xs-9 col-sm-4">
           <a href="/"><img class="img-responsive" src="/assets/site/images/logo_shopping_interlar_aricanduva.png" /></a>
         </div>
-        <div class="col-md-6 col-xs-12" id="bloco-menu">
+        <div class="col-md-6 col-xs-2 col-sm-8" id="bloco-menu">
           <nav class="navbar navbar-default navbar-custom">
             <div class="container-fluid">
               <!-- Brand and toggle get grouped for better mobile display -->
               <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-2" aria-expanded="false">
                   <span class="sr-only">Toggle navigation</span>
                   <span class="icon-bar"></span>
                   <span class="icon-bar"></span>
@@ -24,7 +24,7 @@
               </div>
 
               <!-- Collect the nav links, forms, and other content for toggling -->
-              <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <div class="collapse navbar-collapse hidden-xs" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav">
                   <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><strong>Ambientes </strong><span class="caret"></span></a>
@@ -50,6 +50,32 @@
               </div><!-- /.navbar-collapse -->
             </div><!-- /.container-fluid -->
           </nav>
+        </div>
+        <div class="col-xs-12 visible-xs hidden-md hidden-lg">
+          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-2">
+                <ul class="nav navbar-nav">
+                  <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><strong>Ambientes </strong><span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                      @foreach($ambientes as $id=>$ambiente)
+                      <li> <a href="#" ambiente="{{$id }}" class="bt_ambiente_topo">{{$ambiente}}</a> </li>
+                      @endforeach
+                      
+                    </ul>
+                  </li>
+                  <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><strong>Produtos </strong><span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                      @foreach($tipos as $id=>$tipo)
+                      <li> <a href="#" tipo="{{$id }}" class="bt_tipo_topo">{{$tipo}}</a> </li>
+                      @endforeach
+                    </ul>
+                  </li>                  
+                  <li><a href="{!! URL::to('loja') !!}"><strong>Lojas</strong></a></li>
+                  <li><a href="/localizacao"><strong>Localização</strong></a></li>
+                </ul>
+
+              </div>
         </div>
         <div class="col-md-3 col-xs-12" style="margin-top: 30px;">
           <div class="form-group has-feedback" style="margin-top: 5px;">
